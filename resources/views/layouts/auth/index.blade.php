8b45f4c896cfce92d9464824521b13b9bfb8c1e1
akhmadrizki <akhmadrizki24@gmail.com>
2023-05-16 21:30:04 +0800

    Add layout dashboard

--- resources/views/layouts/auth/index.blade.php
+++ resources/views/layouts/auth/index.blade.php
@@ -1,27 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta charset="UTF-8">
-<meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
-<title>Login &mdash; Stisla</title>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+    <title>Login &mdash; Stisla</title>
 
-<!-- General CSS Files -->
-<link rel="stylesheet" href="{{ asset('dashboard-assets/modules/bootstrap/css/bootstrap.min.css') }}">
-<link rel="stylesheet" href="{{ asset('dashboard-assets/modules/fontawesome/css/all.min.css') }}">
+    <!-- General CSS Files -->
+    <link rel="stylesheet" href="{{ asset('dashboard-assets/modules/bootstrap/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('dashboard-assets/modules/fontawesome/css/all.min.css') }}">
 
-<!-- Template CSS -->
-<link rel="stylesheet" href="{{ asset('dashboard-assets/css/style.css') }}">
-<link rel="stylesheet" href="{{ asset('dashboard-assets/css/components.css') }}">
-<!-- Start GA -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-94034622-3"></script>
-<script>
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-
-gtag('config', 'UA-94034622-3');
-</script>
-<!-- /END GA --></head>
+    <!-- Template CSS -->
+    <link rel="stylesheet" href="{{ asset('dashboard-assets/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('dashboard-assets/css/components.css') }}">
+</head>
 
 <body>
 <div id="app">
---
+++ resources/views/pages/admin/index.blade.php
@@ -0,0 +1,8 @@
+<x-admin::layout title="Dashboard" header="Dashboard">
+    <div class="section-header">
+        <h1>Blank Pages</h1>
+    </div>
+
+    <div class="section-body">
+    </div>
+</x-admin>
